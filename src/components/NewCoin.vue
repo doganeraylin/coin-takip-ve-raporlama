61fe74c4cb59d6e098c1f751024bf470740c1271
doganeraylin <doganeraylinn@gmail.com>
2023-01-07 11:29:48 +0200

    created new coin component

---
+++ src/components/NewCoin.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="new-coin-container">
+        <div class="symbol-info">  
+            <p>symbolname</p>
+            <p>last price</p>
+        </div>
+        <div class="quantity-container">
+            <p>number</p>
+            <div>
+                <p>i</p>
+                <p>d</p>
+            </div>
+        </div>
+        <div class="add-btn">
+            <p>add</p>
+        </div>
+     
+    </div>
+</template>
+
+
+<script>
+
+    export default {
+        
+    }
+</script>
