8c42e4dc026ad743d5fbd1860ee6135b7e26cd8b
doganeraylin <doganeraylinn@gmail.com>
2023-01-07 11:29:58 +0200

    created added coin component

--- src/components/AddedCoin.vue
+++ src/components/AddedCoin.vue
@@ -1,11 +1,36 @@
 <template>
-    <div>
-        
+    <div class="added-coin-container">
+        <div class="symbol-container">
+            <p>symbol name</p>
+            <div class="symbol-info">
+                <p>last price</p>
+                <p>weightedavgprice</p>
+            </div>
+        </div>
+        <div class="quantity-container">
+            <p>number</p>
+            <div>
+                <p>i</p>
+                <p>d</p>
+            </div>
+        </div>
+        <div class="update-remove-container">
+            <p>update</p>
+            <p>remove</p>
+        </div>
+        <!-- <li v-for="coin in coins">
+            <p>{{coin.symbol}}</p>
+        </li> -->
     </div>
 </template>
 
 
 <script>
-
-
+    export default {
+        computed: {
+            products() {
+               
+            }
+        }
+    }
 </script>
