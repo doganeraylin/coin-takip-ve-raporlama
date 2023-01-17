d4332624beba7f22a739a62c4a5cebafaa3b934f
doganeraylin <doganeraylinn@gmail.com>
2023-01-17 20:12:09 +0200

    added methods and styles

--- src/components/AddedCoin.vue
+++ src/components/AddedCoin.vue
@@ -1,36 +1,86 @@
 <template>
-    <div class="added-coin-container">
-        <div class="symbol-container">
-            <p>symbol name</p>
-            <div class="symbol-info">
-                <p>last price</p>
-                <p>weightedavgprice</p>
-            </div>
-        </div>
+    <div class="coin-container">
+        <p>{{ coin.symbol }}</p>
+        <p>{{ coin.lastPrice }}</p>
+        <p>{{ coin. weightedAvgPrice }}</p>
         <div class="quantity-container">
-            <p>number</p>
-            <div>
-                <p>i</p>
-                <p>d</p>
+            <p class="count">{{ count }}</p>
+            <div class="counter-container">
+                <button @click="increment" class="count-btn">+</button>
+                <button @click="decrement" class="count-btn">-</button>
             </div>
         </div>
-        <div class="update-remove-container">
-            <p>update</p>
-            <p>remove</p>
+        <div class="btn-container">
+            <button class="update-btn btn">update</button>
+            <button @click="removeCoin(coin.symbol)" class="remove-btn btn">remove</button>
         </div>
-        <!-- <li v-for="coin in coins">
-            <p>{{coin.symbol}}</p>
-        </li> -->
     </div>
 </template>
 
-
 <script>
+
     export default {
+        props: ['coin'],
+        methods: {
+            removeCoin(symbol) {
+            this.$store.dispatch("removeAddedCoin", symbol);
+            },
+            increment() {
+                this.$store.commit("increment", this.coin.symbol);
+            },
+            decrement() {
+                this.$store.commit("decrement", this.coin.symbol);
+            }, 
+        },
         computed: {
-            products() {
-               
+            count() {
+             return this.coin.count;
             }
-        }
-    }
+        },
+}
+    
 </script>
+
+<style>
+.coin-container {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #EDE9D0;
+    border: 2px solid white;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 5px auto
+}
+.quantity-container {
+    display: flex;
+}
+
+.counter-container {
+    display: flex;
+    flex-direction: column;
+    justify-content:space-around;
+    margin-left: 15px;
+}
+
+.count {
+    background-color: whitesmoke;
+    padding: 5px 10px;
+    border-radius: 2px;
+}
+
+.count-btn {
+    background-color: whitesmoke;
+    border: none;
+    border-radius: 2px;
+    padding: 3px 7px;
+    font-size: 15px;
+}
+
+.update-btn {
+    background-color: #99DFCD;
+}
+.remove-btn {
+    background-color: #E72525;
+}
+</style>
