0a74cc6c321c6056e3e169368683f2898542049a
doganeraylin <doganeraylinn@gmail.com>
2023-01-26 15:09:27 +0200

    added coin info bar

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,7 +1,13 @@
+
 <template>
     <div class="portfolio-container">
         <h1 class="title">My Portfolio</h1>
-        <AddedCoin v-for="coin in addedCoins" :key="coin.symbol" :coin="coin"  :count="coin.count"></AddedCoin>
+        <div class="coin-info-bar">
+            <p>Symbol</p>
+            <p>Last Price</p>
+            <p>Weighted Avg Price</p>
+        </div>
+        <AddedCoin v-for="coin in addedCoins" :key="coin.symbol" :coin="coin"></AddedCoin>
     </div>
 </template>
 
@@ -23,15 +29,35 @@
 </script>
 
 <style scoped>
-.portfolio-container {
-    border: 2px #181818 solid;
+.portfolio-container {   
+    background-color: #222222;
     border-radius: 5px;
     width: 600px;
     padding: 10px;
-    margin: 30px
+    margin: 30px;
 }
 
 .title {
-    color: #181818;
+    margin-left: 20px;
+}
+
+.title,
+.coin-info-bar {
+    color: #F1DFDE;
+}
+
+.coin-info-bar {
+    display: flex;
+    justify-content: space-around;
+    width: 400px;
+    margin-left: 5px;
+}
+
+.coin-info-bar p {
+    border: 2px solid #F1DFDE;
+    padding: 3px 7px;
+    border-radius: 5px;
+    opacity: 0.5;
+    margin-right: 5px;
 }
 </style>
