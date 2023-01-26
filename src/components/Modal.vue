6488a395a952e1cf36e689bba322fc65d41b2b94
doganeraylin <doganeraylinn@gmail.com>
2023-01-26 15:17:21 +0200

    removed scrollbar

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -70,28 +70,54 @@
 
 
 <style scoped>
-    .modal-container {
-        max-width: 500px;
-        max-height: 600px;
-        margin: 0 auto;
-        background-color: #181818;
-        padding: 20px;
-        overflow: scroll;
-        border-radius: 5px;
-    }
-    .search-bar {
-        display: block;
-        padding: 10px;
-        border-radius: 5px;
-    }
-    .close-btn {
-      position:absolute;
-      top: 15px;
-      right: 30px;
-    }
+  .modal-container {
+      width: 600px;
+      max-height: 600px;
+      margin: 0 auto;
+      background-color: #181818;
+      padding: 20px;
+      overflow: scroll;
+      border-radius: 5px;
+      scrollbar-color: green;
+  }
 
-    .close-btn:hover {
-      cursor: pointer;
-      background-color: rgb(173, 4, 4);
-    }
+  .modal-container::-webkit-scrollbar {
+    display: none;
+  }
+  .search-bar {
+      display: block;
+      padding: 10px;
+      border-radius: 5px;
+      margin-bottom: 15px;
+      margin: 10px 0 10px 15px;
+  }
+  .close-btn {
+    position:absolute;
+    top: 30px;
+    right: 25px;
+    padding: 5px 10px;
+    border-radius: 3px;
+    background-color: white;
+  }
+
+  .close-btn:hover {
+    cursor: pointer;
+    background-color: #E86A34;
+  }
+
+  .coin-info-bar {
+  display: flex;
+  justify-content: space-around;
+  width: 400px;
+  margin-left: 5px;
+  }
+
+  .coin-info-bar p {
+    color: #F1DFDE;
+    border: 2px solid #F1DFDE;
+    padding: 3px 7px;
+    border-radius: 5px;
+    opacity: 0.5;
+    margin-right: 5px;
+  }
 </style>
